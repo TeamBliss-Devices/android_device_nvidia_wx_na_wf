660aa26d427e03c9362b84da5f32ed23fc9f8252
electrikjesus <electrikjesus@gmail.com>
2015-06-27 15:17:14 -0400

    Blissified

--- bliss.mk
+++ bliss.mk
@@ -1,8 +1,8 @@
 # Inherit device configuration for wx_na_wf.
 $(call inherit-product, device/nvidia/wx_na_wf/full_wx_na_wf.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=wx_na_wf \
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="nvidia/wx_na_wf/shieldtablet:5.0.1/LRX22C/29082_493.9700:user/release-keys" \
     PRIVATE_BUILD_DESC="wx_na_wf-user 5.0.1 LRX22C 29082_493.9700 release-keys"
 
-PRODUCT_NAME := cm_wx_na_wf
+PRODUCT_NAME := bliss_wx_na_wf
 PRODUCT_DEVICE := wx_na_wf
