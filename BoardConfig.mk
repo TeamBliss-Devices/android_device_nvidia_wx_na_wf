0b2a1a263c2a774c340f149e5f36882788aefdd3
BitO BSessiOn <bitobsession@gmail.com>
2015-07-17 10:02:43 +0200

    Change config for compilation with Sabermod
    KERNEL: Sabermod 4.9
    ROM: Sabermod 4.8
    
    Change-Id: Ie862499085b50bd80345dff7b974bcde14c6f41a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,8 +27,8 @@
 TARGET_OTA_ASSERT_DEVICE := shieldtablet
 
 # BlissPop Configs
-TARGET_TC_ROM := 4.8-linaro
-TARGET_TC_KERNEL := 4.8-linaro
+TARGET_TC_ROM := 4.8-sm
+TARGET_TC_KERNEL := 4.9-sm
 BLISSIFY := true
 BLISS_O3 := true
 BLISS_STRICT := false
